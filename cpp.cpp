3f9f9acdb12a6f03e830a7d9f7f8554f4663f841
Ates Ates <ateschatesch2020@gmail.com>
2023-02-18 07:54:11 +0100

    fooFunctionAdded

--- cpp.cpp
+++ cpp.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+void foo()
+{
+    std::cout << "Leyla!" << std::endl;    
+}
 int main()
 {
     std::cout << "Ates!" << std::endl;
     std::cout << "Sema!" << std::endl;
+    foo();
 }
+
