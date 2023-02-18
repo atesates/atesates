a7c96b4442ff7250d53468e45ad3719edce37ec2
Ates Ates <ateschatesch2020@gmail.com>
2023-02-18 07:56:26 +0100

    barFunctionAdded

--- cpp.cpp
+++ cpp.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+void bar()
+{
+    std::cout << "Mina!" << std::endl;
+
+}
+
 int main()
 {
     std::cout << "Ates!" << std::endl;
     std::cout << "Sema!" << std::endl;
+    bar();
 }
