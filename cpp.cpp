7c6a10e7bfe811d421e39d9c615ad71576d6146a
atesates <ateschatesch2020@gmail.com>
2023-03-09 17:07:43 +0100

    zero init example added

--- cpp.cpp
+++ cpp.cpp
@@ -5,11 +5,34 @@ void bar()
     std::cout << "Mina!" << std::endl;
 
 }
+  class Myclass{
+    public:
+    Myclass() = default;
+    // Myclass(){
+    //   std::cout << "ctor this: " << this << std::endl;
 
+    // };
+    ~Myclass(){
+      std::cout << "dtor this: " << this << std::endl;
+
+    }
+        int a;
+        bool flag;
+    };
 int main()
 {
     std::cout << "Ates!" << std::endl;
     std::cout << "Semaaaa!" << std::endl;
     std::cout << "Semaaa!" << std::endl;
     bar();
+
+
+    //Myclass myclass;
+    Myclass myclass{};
+    //myclass.a = 3;
+    std::cout << "myclass.a= " << myclass.a << std::endl;
+    std::cout << "myclass.flag= " << myclass.flag << std::endl;
+ 
+
+
 }
